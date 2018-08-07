349f3ac10af5899b8e68d2c18067e49a0fe9c425
cyh-etlab <41722972+cyh-etlab@users.noreply.github.com>
2018-08-07 16:15:58 +0800

    Add files via upload

---
+++ Blink12.ino
@@ -0,0 +1,59 @@
+#include <Arduino_FreeRTOS.h>
+// define two tasks for Blink1 & Blink2
+void TaskBlink1( void *pvParameters );
+void TaskBlink2( void *pvParameters );
+// the setup function runs once when you press reset or power the board
+void setup() {  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+  // Now set up two tasks to run independently.
+  xTaskCreate(
+    TaskBlink1
+    ,  (const portCHAR *)"Blink1"   // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL );
+
+  xTaskCreate(
+    TaskBlink2
+    ,  (const portCHAR *) "Blink2"
+    ,  128  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+}
+void loop()
+{
+  // Empty. Things are done in Tasks.
+}
+/*---------------------- Tasks ---------------------*/
+void TaskBlink1(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+  // initialize digital LED_BUILTIN on pin 13 as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  for (;;) // A Task shall never return or exit.
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+  }
+}
+void TaskBlink2(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+  pinMode(12, OUTPUT); // initialize digital LED on pin 12 as an output.
+  for (;;)
+  {
+    digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for one second
+  }
+}
